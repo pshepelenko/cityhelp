be0d2aec3deed1da350ab5b3fc34ff60c0de3186
Шепеленко Павел Алексеевич <shepelenko@aoglonass.ru>
2021-09-01 10:18:20 +0300

    Deleted a link to cookie-parser from app.js

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+
 var logger = require('morgan');
 const cors = require('cors');
 var bodyParser = require('body-parser');
